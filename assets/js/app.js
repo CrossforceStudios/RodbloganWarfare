e8c87a9e074e0768436d23c1455097cabfa5b851
Aerodos12 <30639238+Aerodos12@users.noreply.github.com>
2023-11-13 17:35:01 -0500

    jQuery Switch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,24 @@
 
 
-let navBtns = document.querySelectorAll(".rw-nav-menu-btn");
+let navBtns = document.querySelectorAll("");
 
 navBtns.forEach(function(ele) {
     ele.addEventListener("click", function() {
-        let navMenu = document.getElementById(ele.dataset.navid);
 
         ele.classList.toggle("active");
 
         navMenu.classList.toggle("active");
     });
 });
+$(function(){
+    $('.has-sub').on('click', function(e) { // Get all dropdown menu toggles
+        $('.dropdown-menu').not($(this).children('.dropdown-menu')).removeClass('dropdown-shown'); // Hide all other dropdown menus
+        $('.has-sub').not($(this)).removeClass('active'); // Remove the active selector from the other dropdown toggles
+        $(this).children('.dropdown-menu').toggleClass('dropdown-shown'); // Show/hide the dropdown menu associated with the toggle being clicked
+        $(this).toggleClass('active'); // Toggle the active selector on the nav-item
+    });
+    $('.rw-nav-menu-btn').on('click', function(e) { // Get all dropdown menu toggles
+        $(this.dataset.navid).toggleClass('active'); // Hide all other dropdown menus
+        $(this).toggleClass('active'); // Hide all other dropdown menus
+    });
+  });
