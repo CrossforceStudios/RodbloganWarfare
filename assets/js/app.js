1e1bf73f85e336aa154b825e953c73d91d029e12
Aerodos12 <30639238+Aerodos12@users.noreply.github.com>
2023-11-13 17:15:56 -0500

    Fix Nav Menu (Mobile)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 
+
+let navBtns = document.querySelector(".rw-nav-menu-btn");
+
+navBtns.forEach(function(ele) {
+    ele.addEventListener("click", function() {
+        let navMenu = document.getElementById(ele.dataset.navid);
+
+        ele.classList.toggle("active");
+
+        navMenu.classList.toggle("active");
+    });
+});
